6d754863ff5cfbac40548a5bc203b6471e622a7a
NikollaLaskuDev <nikolla.lasku@gmail.com>
2026-05-08 20:47:44 +0200

    Add .gitignore and logging system to Calc.py

--- calc/Calc.py
+++ calc/Calc.py
@@ -1,25 +1,37 @@
+import logging
+logging.basicConfig(filename='calc.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 print('Enter a number, an operator and a second number to start')
+logging.debug('Start of Calc.py')
 
 while True:
     user_input1 = input('>')
     if user_input1 == 'q':
         print('You have successfully finished calculating')
+        logging.info('User has quit the program')
         break
     user_input2 = input('>') #Operator (+,-,*./)
     user_input3 = input('>')
 
     try:
         if user_input2 == '+':
-            print(user_input1, user_input2, user_input3, '=', int(user_input1) + int(user_input3))
+            logging.debug(f'Calculation: {user_input1} + {user_input2} + {user_input3} = {int(user_input1) + int(user_input3)}')
+            print(f'{user_input1} {user_input2} {user_input3} = {int(user_input1) + int(user_input3)}')
         elif user_input2 == '-':
-            print(user_input1, user_input2, user_input3, '=', int(user_input1) - int(user_input3))
+            logging.debug(f'Calculation: {user_input1} - {user_input2} - {user_input3} = {int(user_input1) - int(user_input3)}')
+            print(f'{user_input1} {user_input2} {user_input3} = {int(user_input1) - int(user_input3)}')
         elif user_input2 == '*':
-            print(user_input1, user_input2, user_input3, '=', int(user_input1) * int(user_input3))
+            logging.debug(f'Calculation: {user_input1} * {user_input2} * {user_input3} = {int(user_input1) * int(user_input3)}')
+            print(f'{user_input1} {user_input2} {user_input3} = {int(user_input1) * int(user_input3)}')
         elif user_input2 == '/':
-            print(user_input1, user_input2, user_input3, '=', int(user_input1) / int(user_input3))
+            logging.debug(f'Calculation: {user_input1} / {user_input2} / {user_input3} = {int(user_input1) / int(user_input3)}')
+            print(f'{user_input1} {user_input2} {user_input3} = {int(user_input1) / int(user_input3)}')
         else:
-             print('Invalid operator. Please only input +, -, *, or /')
+            logging.warning(f'Invalid operator entered: {user_input2}')
+            print('Invalid operator. Please only input +, -, *, or /')
     except ValueError:
-            print('Invalid input. Please only input numbers')
+           logging.error(f'ValueError: input1={user_input1}, input2={user_input2}, input3={user_input3}')
+           print('Invalid input. Please only input numbers')
     except ZeroDivisionError:
+         logging.error(f'ZeroDivisionError: input1={user_input1}, input2={user_input2}, input3={user_input3}')
          print('Cannot divide by zero. Please input a non-zero number for the second number when using division.')
